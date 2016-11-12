26058b00c51bde6a73414503bdfa5e9c2aa106b2
Chaz-C <chazcastro@Chazs-MacBook-Pro.local>
2016-11-12 13:31:13 -1000

    finished

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,11 @@
 'use strict';
 module.exports = function() {
-  
+
+  var ranNum = 0;
+  ranNum = Math.floor(Math.random() * 1000000);
+
+  return function getNum() {
+
+    return ranNum;
+  };
 };
